4e8df829e82c2f9fd9e19e300158093cbb45515f
Calvin Hendrawan <tugasakun22@gmail.com>
2023-12-28 20:06:37 +0700

    menambahkan object di dalam InsertScreen.kt

--- app/src/main/java/com/example/databaseapi/ui/kontak/screen/InsertScreen.kt
+++ app/src/main/java/com/example/databaseapi/ui/kontak/screen/InsertScreen.kt
@@ -1,2 +1,8 @@
 package com.example.databaseapi.ui.kontak.screen
 
+import com.example.databaseapi.navigation.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Entry Siswa"
+}
