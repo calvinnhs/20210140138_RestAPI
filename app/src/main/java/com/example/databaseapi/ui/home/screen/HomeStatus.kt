3d73a596de82e8112bb67a05da46ad9e432badc9
Calvin Hendrawan <tugasakun22@gmail.com>
2023-12-28 19:37:09 +0700

    menambahkan fun OnError

--- app/src/main/java/com/example/databaseapi/ui/home/screen/HomeStatus.kt
+++ app/src/main/java/com/example/databaseapi/ui/home/screen/HomeStatus.kt
@@ -1,10 +1,16 @@
 package com.example.databaseapi.ui.home.screen
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -43,4 +49,25 @@ fun OnLoading(modifier: Modifier = Modifier){
         painter = painterResource(R.drawable.loading_img),
         contentDescription = stringResource(R.string.loading)
     )
+}
+
+
+@Composable
+fun OnError(retryAction: () -> Unit, modifier: Modifier = Modifier){
+    Column (
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.ic_connection_error),
+            contentDescription = ""
+        )
+        Text(
+            text = stringResource(id = R.string.loading_failed), modifier = Modifier.padding(16.dp))
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+
+        }
+    }
 }
