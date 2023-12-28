f0e3e6afb972c518573bd11af4d8fb0a32641e37
Calvin Hendrawan <tugasakun22@gmail.com>
2023-12-28 19:35:06 +0700

    menambahkan fun OnLoading

--- app/src/main/java/com/example/databaseapi/ui/home/screen/HomeStatus.kt
+++ app/src/main/java/com/example/databaseapi/ui/home/screen/HomeStatus.kt
@@ -1,5 +1,15 @@
 package com.example.databaseapi.ui.home.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.databaseapi.R
 import com.example.databaseapi.model.Kontak
 import com.example.databaseapi.ui.home.viewmodel.KontakUIState
 
@@ -24,4 +34,13 @@ fun HomeStatus(
 
         is KontakUIState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
     }
+}
+
+@Composable
+fun OnLoading(modifier: Modifier = Modifier){
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.loading_img),
+        contentDescription = stringResource(R.string.loading)
+    )
 }
