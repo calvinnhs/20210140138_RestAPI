9882221a03cfed3a4340020e57dac08a2d677714
Calvin Hendrawan <tugasakun22@gmail.com>
2023-12-28 19:38:06 +0700

    menambahkan fun KontakLayout

--- app/src/main/java/com/example/databaseapi/ui/home/screen/HomeStatus.kt
+++ app/src/main/java/com/example/databaseapi/ui/home/screen/HomeStatus.kt
@@ -1,12 +1,16 @@
 package com.example.databaseapi.ui.home.screen
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -67,6 +71,25 @@ fun OnError(retryAction: () -> Unit, modifier: Modifier = Modifier){
             text = stringResource(id = R.string.loading_failed), modifier = Modifier.padding(16.dp))
         Button(onClick = retryAction) {
             Text(stringResource(R.string.retry))
+
+        }
+    }
+}
+
+@Composable
+fun KontakLayout(
+    kontak: List<Kontak>,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(kontak) { kontak ->
+            KontakCard(kontak = kontak, modifier = Modifier
+                .fillMaxWidth()
+                .clickable {})
 
         }
     }
